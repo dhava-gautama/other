2d896820ed264dff8a8cd9a984cf90feb74508d4
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:44:41 +0700

    Update 100year.py

--- 100year.py
+++ 100year.py
@@ -1,8 +1,8 @@
-import datetime
+import datetime # Modul untuk mendapat tahun saat ini
 
-name = input('Siapa namamu?')
-usia = int(input('Berapa usiamu saat ini?'))
-tahun_sekarang = datetime.date.today().year
-tahunusia100 = tahun_sekarang-usia+100
-print("Halo", name)
-print('Kamu akan berusia 100 tahun di tahun',tahunusia100)
+name = input('Siapa namamu?') #Meminta user untuk memasukkan namanya
+usia = int(input('Berapa usiamu saat ini?')) #Meminta user untuk memasukan usianya saat ini convert menjadi integer
+tahun_sekarang = datetime.date.today().year #Mendapatkan tahun saat ini
+tahunusia100 = tahun_sekarang-usia+100 #Menghitung tahun saat usia user 100 tahun
+print("Halo", name) #Menyapa user
+print('Kamu akan berusia 100 tahun di tahun',tahunusia100) #Memberi tahu kapan user berusia 100 tahun
