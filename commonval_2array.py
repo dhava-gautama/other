d58940ae81c7f882281128fa5c1ac0b62ba75cd6
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:54:27 +0700

    Update commonval_2array.py

--- commonval_2array.py
+++ commonval_2array.py
@@ -1,3 +1,8 @@
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]. 
+Buat program python untuk mencari elemen-elemen yang ada di list a dan list b tanpa pengulangan.
+'''
+
 import numpy # Mengimport modul numpy
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] #Array ke-1
