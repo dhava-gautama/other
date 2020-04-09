e893d6c2bc6528ea202f8b2bb7534521ab1d22ab
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:46:42 +0700

    Update bilangan_pembagi.py

--- bilangan_pembagi.py
+++ bilangan_pembagi.py
@@ -1,6 +1,6 @@
 print('Program pencari pembagi angka')
-number = int(input('Masukan angka'))
+number = int(input('Masukan angka')) #Meminta user untuk menginput angka yang akan dicari pembaginya
 print('Pembagi bilangan tersebut adalah')
-for i in range(1,number):
-    if number%i == 0:
-        print(i)
+for i in range(1,number+1): #Untuk angka range 1-number
+    if number%i == 0: #Bila sisa bagi = 0 maka bilangan tersebut adalah bilangan pembagi
+        print(i) #Print bilangan pembagi
