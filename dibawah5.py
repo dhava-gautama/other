933d5495a58b30df152ef7c87d28d16096dbe812
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:50:34 +0700

    Update dibawah5.py

--- dibawah5.py
+++ dibawah5.py
@@ -1,4 +1,4 @@
-data = [2, 3, 5, 7, 8, 11, 11, 3, 5, 6, 0.3, 0.44, 20, 100]
-for i in range(0,len(data)):
-    if data[i] <=5:
-        print (data[i])
+data = [2, 3, 5, 7, 8, 11, 11, 3, 5, 6, 0.3, 0.44, 20, 100] #Membuat array
+for i in range(0,len(data)): #Looping
+    if data[i] <=5: #Bila nilai ke-1 pada array data kurang dari 5 maka
+        print (data[i]) #Print nilai
