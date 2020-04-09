223d8c89155ac76b81ee33b86a93e466094c8fa5
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:23:07 +0700

    Create ganjil_genap.py

---
+++ ganjil_genap.py
@@ -0,0 +1,6 @@
+print("Angkamu ganjil atau genap yaa.. Yuk kita check")
+number = int(input('Masukan angka yang ingin anda check'))
+if number%2 == 0:
+    print('Angkamu genap')
+else:
+    print('Angkamu ganjil')
