6761f93ea255ef191abd11d29de3d9e85b0c5746
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:51:39 +0700

    Update ganjil_genap.py

--- ganjil_genap.py
+++ ganjil_genap.py
@@ -1,6 +1,6 @@
 print("Angkamu ganjil atau genap yaa.. Yuk kita check")
-number = int(input('Masukan angka yang ingin anda check'))
-if number%2 == 0:
+number = int(input('Masukan angka yang ingin anda check')) #Meminta user untuk memasukan angka
+if number%2 == 0: # Bila sisa bagi angka/2 = 0 maka bilangan tersebut genap
     print('Angkamu genap')
-else:
+else: # Bila tidak bilangan tersebut ganjil
     print('Angkamu ganjil')
