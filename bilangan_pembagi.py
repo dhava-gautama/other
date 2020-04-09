3c9721c8247afb21bfe638275d6f01949136abfd
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:53:03 +0700

    Update bilangan_pembagi.py

--- bilangan_pembagi.py
+++ bilangan_pembagi.py
@@ -1,3 +1,9 @@
+'''
+Buat program yang meminta user untuk memasukkan (input) sebuah angka. Cetak semua pembagi
+dari angka tersebut, disini pembagi adalah bilangan yang membagi habis bilangan yang diinput user.
+Contoh: 6 pembaginya adalah 6, 3, 2, dan 1.
+'''
+
 print('Program pencari pembagi angka')
 number = int(input('Masukan angka')) #Meminta user untuk menginput angka yang akan dicari pembaginya
 print('Pembagi bilangan tersebut adalah')
