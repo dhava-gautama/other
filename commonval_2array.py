29fbfdaa57d605c671462524a2a3424d771da8d8
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:48:53 +0700

    Update commonval_2array.py

--- commonval_2array.py
+++ commonval_2array.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy # Mengimport modul numpy
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] #Array ke-1
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] #Array ke-2
+c = numpy.intersect1d(a,b) #Mencari elemen sama yang berada pada array a dan array b
 
-print(numpy.intersect1d(a,b))
+print(c) #Print element array yang sama
